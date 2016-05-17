e102a74b08e38636e43ceb0902a3856325768391
jvhoven <wolferine@live.se>
2016-05-17 18:48:31 +0200

    Responsive css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,7 @@ export class AppComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit() {
-      this.router.navigate(['/']);
+      //this.router.navigate(['/']);
   }
 
 }
