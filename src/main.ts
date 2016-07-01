6747b76a15e593e4b317de1c7a1a0583a83029fe
jvhoven <jeffreyvanhoven@gmail.com>
2016-07-01 12:21:32 +0200

    Updated favicon and added .htaccess to enable html 5 stuff

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provide } from '@angular/core';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common'
 import { AppComponent, environment } from './app/';
 import { APP_ROUTER_PROVIDERS } from './app/app.routes';
 import { HTTP_PROVIDERS } from '@angular/http';
 import 'rxjs/Rx';
 
 enableProdMode();
-bootstrap(AppComponent, [APP_ROUTER_PROVIDERS, HTTP_PROVIDERS]);
+bootstrap(AppComponent, [{provide: LocationStrategy, useClass: HashLocationStrategy }, APP_ROUTER_PROVIDERS, HTTP_PROVIDERS]);
